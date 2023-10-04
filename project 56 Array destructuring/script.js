6147e586cda6c0df18cf73667d6de31c2b4f0d97
Haroon <haroonnadir143@gmail.com>
2023-10-04 16:54:43 +0500

    first commit

---
+++ project 56 Array destructuring/script.js
@@ -0,0 +1,4 @@
+const numbers = [10, 20, 30];
+const [firstNumber, secondNumber] = numbers;
+console.log('First number:', firstNumber);   // Outputs: 10
+console.log('Second number:', secondNumber); // Outputs: 20
